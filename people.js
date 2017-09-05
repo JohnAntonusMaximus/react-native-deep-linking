953d97762fc057f5182a99e7f0d4fdfa3801d6ba
KaizenTek <kaizentek@KaizenTeks-MacBook-Air.local>
2017-09-05 13:04:56 -0400

    Deep Linking ios working

--- people.js
+++ people.js
@@ -3,21 +3,21 @@ import { Text, Image, View } from 'react-native';
 
 const people = {
     0: {
-    name: 'Leela',
-    image: 'http://vignette1.wikia.nocookie.net/en.futurama/images/d/d4/Turanga_Leela.png/revision/latest?cb=20150218013044',
-  },
-  1: {
-    name: 'Bender',
-    image: 'https://vignette2.wikia.nocookie.net/en.futurama/images/4/43/Bender.png/revision/latest?cb=20150206072725',
-  },
-  2: {
-    name: 'Amy',
-    image: 'https://i.ytimg.com/vi/4sCtTq7K3yI/hqdefault.jpg',
-  },
-  3: {
-    name: 'Fry',
-    image: 'http://www.supergrove.com/wp-content/uploads/2017/03/fry-futurama-22-which-robot-from-quotfuturamaquot-are-you.jpg',
-  }
+        name: 'Leela',
+        image: 'https://vignette1.wikia.nocookie.net/en.futurama/images/d/d4/Turanga_Leela.png/revision/latest?cb=20150218013044',
+    },
+    1: {
+        name: 'Bender',
+        image: 'https://vignette2.wikia.nocookie.net/en.futurama/images/4/43/Bender.png/revision/latest?cb=20150206072725',
+    },
+    2: {
+        name: 'Amy',
+        image: 'https://i.ytimg.com/vi/4sCtTq7K3yI/hqdefault.jpg',
+    },
+    3: {
+        name: 'Fry',
+        image: 'http://www.supergrove.com/wp-content/uploads/2017/03/fry-futurama-22-which-robot-from-quotfuturamaquot-are-you.jpg',
+    }
 };
 
 class People extends Component{
