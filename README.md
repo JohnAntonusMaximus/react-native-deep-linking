a9af15fd7ce6cb88dfd6fe6b61d6d53ee8faadf3
KaizenTek <jradosta@kaizentek.io>
2017-09-05 11:17:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native Deep Linking
---
+++ home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Platform, Text, Linking } from 'react-native';
+
+class Home extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    }
+
+    componentDidMount(){
+        if(Platform.OS === 'android'){
+            Linking.getInitialURL()
+                .then(url => this.navigate(url));
+        } else {
+            Linking.addEventListener('url', this.handleOpenURL);
+        }
+    }
+
+    componentWillUnMount(){
+        Linking.removeEventListener('url', this.handleOpenURL);
+    }
+
+    handleOpenURL = (event) => {
+        this.navigate(event.url);
+    }
+
+    navigate = (url) => {
+        const { navigate } = this.props.navigation;
+        const route = url.replace(/.*?:\/\//g, '');
+        const id = route.match(/\/([^\/]+)\/?$/)[1];
+        const routeName = route.split('/')[0];
+
+        if(routeName === 'people'){
+            navigate('People', { id, name: 'Chris'});
+        }
+    }
+
+    render(){
+        return <Text>Hello from Home!</Text>
+    }
+}
+
+export default Home;
